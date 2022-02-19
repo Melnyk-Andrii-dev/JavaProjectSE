4a5064f3853b788b2a42a9450a710a8f6978a205
Volodymyr_Hrinchenko <vhrinchenko@dashfinancial.com>
2022-02-19 16:17:03 +0200

    feedback hw 22

--- src/main/java/ua/intita/qa/homework22/MinMaxThreads.java
+++ src/main/java/ua/intita/qa/homework22/MinMaxThreads.java
@@ -1,5 +1,8 @@
 package ua.intita.qa.homework22;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.*;
 
 public class MinMaxThreads {
@@ -10,31 +13,53 @@ public class MinMaxThreads {
             mass[i] = ThreadLocalRandom.current().nextInt(-100000000, 100000000);
         }
 
-        ExecutorService minFinderThread = Executors.newFixedThreadPool(3);
-        Future<Integer> min = minFinderThread.submit(() -> {
+//        ExecutorService minFinderThread = Executors.newFixedThreadPool(3);
+//        Future<Integer> min = minFinderThread.submit(() -> {
+//            int min1 = Integer.MAX_VALUE;
+//            for (int j : mass) {
+//                if (min1 > j) min1 = j;
+//            }
+//            return min1;
+//        });
+//
+//        ExecutorService maxFinderThread = Executors.newFixedThreadPool(3);
+//        Future<Integer> max = maxFinderThread.submit(new MaxFinderThread(mass));
+
+        List<Callable<Integer>> tasks = new ArrayList<>(2);
+        Callable<Integer> task = () -> {
             int min1 = Integer.MAX_VALUE;
             for (int j : mass) {
                 if (min1 > j) min1 = j;
             }
             return min1;
-        });
-
-        ExecutorService maxFinderThread = Executors.newFixedThreadPool(3);
-        Future<Integer> max = maxFinderThread.submit(new MaxFinderThread(mass));
-
+        };
+        tasks.add(task);
+        tasks.add(new MaxFinderThread(mass));
+        ExecutorService service = Executors.newFixedThreadPool(2);
         try {
-            System.out.println("MIN: " + min.get());
+            List<Future<Integer>> futures = service.invokeAll(tasks);
+            for (Future<Integer> future : futures) {
+                System.out.println(future.get());
+            }
+
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
         }
+        service.shutdown();
 
-        try {
-            System.out.println("MAX: " + max.get());
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-        }
-        minFinderThread.shutdown();
-        maxFinderThread.shutdown();
+//        try {
+//            System.out.println("MIN: " + min.get());
+//        } catch (InterruptedException | ExecutionException e) {
+//            e.printStackTrace();
+//        }
+//
+//        try {
+//            System.out.println("MAX: " + max.get());
+//        } catch (InterruptedException | ExecutionException e) {
+//            e.printStackTrace();
+//        }
+//        minFinderThread.shutdown();
+//        maxFinderThread.shutdown();
 
     }
 }
